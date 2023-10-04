f8be49f916290fbd1d40bd9e3ec761be54270fa9
MrNereof <mrnereof@gmail.com>
2023-10-04 12:22:14 +0300

    Oopss

--- manage.py
+++ manage.py
@@ -6,16 +6,18 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HistoryBot.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+
+    with open('file.txt', 'w') as sys.stdout:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HistoryBot.settings')
+        try:
+            from django.core.management import execute_from_command_line
+        except ImportError as exc:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            ) from exc
+        execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
